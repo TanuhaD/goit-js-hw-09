1084e7f3060431cedd812312b0ea0dcce41ac2a7
TanuhaD <demchenkotanuha@gmail.com>
2022-11-12 21:39:04 +0200

    change

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,23 +1,30 @@
 import { getRandomHexColor } from './utils/utils';
-const buttonStart = document.querySelector('[data-start]');
-const buttonStop = document.querySelector('[data-stop]');
-const body = document.querySelector('body');
-let timerId = null;
-function handleStartClick() {
-  timerId = setInterval(() => {
-    body.style.backgroundColor = getRandomHexColor();
-  }, 1000);
-  toggleButtons();
-}
-function handleStopClick() {
-  clearInterval(timerId);
-  toggleButtons();
-}
-function toggleButtons() {
-  buttonStart.disabled = !buttonStart.disabled;
-  buttonStop.disabled = !buttonStop.disabled;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-buttonStop.disabled = true;
-buttonStart.addEventListener('click', handleStartClick);
-buttonStop.addEventListener('click', handleStopClick);
+const buttonStartEl = document.querySelector('[data-start]');
+const buttonStopEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+
+function handleStartClick(event) {
+  toggleButtonStatus();
+  intervalId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function handleStopClick(event) {
+  clearInterval(intervalId);
+  toggleButtonStatus();
+}
+
+function toggleButtonStatus() {
+  buttonStopEl.disabled = !buttonStopEl.disabled;
+  buttonStartEl.disabled = !buttonStartEl.disabled;
+}
+
+buttonStopEl.disabled = true;
+buttonStartEl.addEventListener('click', handleStartClick);
+buttonStopEl.addEventListener('click', handleStopClick);
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,24 +1,22 @@
 import flatpickr from 'flatpickr';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import 'flatpickr/dist/flatpickr.min.css';
 import { options } from './utils/options.js';
+import 'flatpickr/dist/flatpickr.min.css';
 import { convertMs } from './utils/utils.js';
 const buttonStartEl = document.querySelector('[data-start]');
-let daysEl = document.querySelector('[data-days]');
-let hoursEl = document.querySelector('[data-hours]');
-let minutesEl = document.querySelector('[data-minutes]');
-let secondsEl = document.querySelector('[data-seconds]');
-let selectedDate = null;
-let timerId = null;
-flatpickr('#datetime-picker', {
+const inputCalendar = document.querySelector('#datetime-picker');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let selectedDate;
+flatpickr(inputCalendar, {
   ...options,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    console.log(Date.now());
-    selectedDate = +selectedDates[0];
-    const difference = Date.now() - selectedDate;
-    if (difference > 0) {
-      // alert('Please choose a date in the future');
+    selectedDate = selectedDates[0];
+    const difference = selectedDate - Date.now();
+    if (difference < 0) {
       Notify.info('Please choose a date in the future');
       return;
     }
@@ -29,19 +27,17 @@ flatpickr('#datetime-picker', {
 function handleStartClick() {
   buttonStartEl.disabled = true;
   timerId = setInterval(() => {
-    const time = selectedDate - Date.now();
-    showTime(convertMs(time));
-    if (time < 1100) {
-      clearInterval(timerId);
-    }
+    const timeLeft = selectedDate - Date.now();
+    const objectTime = convertMs(timeLeft);
+    showTime(objectTime);
   }, 1000);
 }
+
 function showTime({ days, hours, minutes, seconds }) {
   daysEl.textContent = ('' + days).padStart(2, '0');
   hoursEl.textContent = ('' + hours).padStart(2, '0');
   minutesEl.textContent = ('' + minutes).padStart(2, '0');
   secondsEl.textContent = ('' + seconds).padStart(2, '0');
-  // padStart(2,'0');
 }
 buttonStartEl.disabled = true;
 
